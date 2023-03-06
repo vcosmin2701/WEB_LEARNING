f63ede593e482c07f44e901dbc96c8dbb4d0b5f4
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-06 22:58:53 +0200

    New features

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,42 @@
-let connection = true;
-if(connection){
-    alert("You've been connected to my page!");
-}
-
 function points(score, interval){
     let result = (score * 50) + (interval / 2);
     return result;
-};
+}
 
 const myImage = document.querySelector("img");
+
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
-    if(mySrc === "images/newAndroid.png"){
+    if(mySrc === "images/androidPhoto.png"){
         myImage.setAttribute("src", "images/githubLogo.jpg");
     }else{
         myImage.setAttribute("src", "images/androidPhoto.png");
     }
-};
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Have a good day, ${myName}`;
+    }
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+}else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Still have a great day, ${storedName}`;
+}
+
+
+
