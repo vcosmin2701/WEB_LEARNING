b19855a42ba9df2e9b6d8817f0fa60a66ee14fcd
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-06 16:21:56 +0200

    Basic JavaScript functionality
    Going through basic JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let connection = true;
+if(connection){
+    alert("You've been connected to my page!");
+}
+
+function points(score, interval){
+    let result = (score * 50) + (interval / 2);
+    return result;
+}
