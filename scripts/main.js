0b7c9926003a857367ce776a036a4cad75a6e82e
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-06 22:25:27 +0200

    New items

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,14 @@ if(connection){
 function points(score, interval){
     let result = (score * 50) + (interval / 2);
     return result;
-}
+};
+
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if(mySrc === "images/newAndroid.png"){
+        myImage.setAttribute("src", "images/githubLogo.jpg");
+    }else{
+        myImage.setAttribute("src", "images/androidPhoto.png");
+    }
+};
